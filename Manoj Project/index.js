6ecca4a699397077272533906feb8c14f74fd233
Keerthan TH <keerthankeerthu001@gmail.com>
2025-01-30 13:45:51 +0530

    Fixed line endings

---
+++ Manoj Project/index.js
@@ -0,0 +1,32 @@
+document.getElementById("contactForm").addEventListener("submit", async function (event) {
+    event.preventDefault();
+  
+    const formData = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      mobile: document.getElementById("mobile").value,
+      message: document.getElementById("message").value,
+    };
+    
+    console.log("Form Data:", formData);
+    console.log("Submitting form...");
+
+    try {
+      const response = await fetch("http://192.168.1.7:3000/api/send-email", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+  
+      const result = await response.json();
+      if (result.success) {
+        alert("Email sent successfully!");
+      } else {
+        alert("Failed to send email. Please try again.");
+      }
+    } catch (error) {
+      alert("An error occurred. Please try again.");
+    }
+  });
