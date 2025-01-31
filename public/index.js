39931ea140c9b258117d8e51c7247005ff97643e
KeerthanTH <157266981+KeerthanTH@users.noreply.github.com>
2025-01-31 15:01:36 +0530

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,39 +1,38 @@
 document.getElementById("contactForm").addEventListener("submit", async function (event) {
-    event.preventDefault();
-  
-    const formData = {
-      name: document.getElementById("name").value,
-      email: document.getElementById("email").value,
-      mobile: document.getElementById("mobile").value,
-      message: document.getElementById("message").value,
-    };
-    
-    console.log("Form Data:", formData);
-    console.log("Submitting form...");
+  event.preventDefault();
 
-    try {
-      const response = await fetch("/.netlify/functions/hello-world", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });   
-      console.log("Response:", response);
-  
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-  
-      const result = await response.json();
-      if (result.sucess) {
-        alert("Email sent successfully!");
-      } else {
-        alert("Failed to send email. Please try again.");
-      }
+  const formData = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    mobile: document.getElementById("mobile").value,
+    message: document.getElementById("message").value,
+  };
 
-    } catch (error) {
-      console.error("Error:", error);
-      alert("An error occurred. Please try again.");
+  console.log("Form Data:", formData);
+  console.log("Submitting form...");
+
+  try {
+    const response = await fetch("/.netlify/functions/hello-world", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
-  });
+
+    const result = await response.json();
+    if (result.success) {
+      alert("Email sent successfully!");
+    } else {
+      alert("Failed to send email. Please try again.");
+    }
+
+  } catch (error) {
+    console.error("Error:", error);
+    alert("An error occurred. Please try again.");
+  }
+});
